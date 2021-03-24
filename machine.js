6171469128b7cf65127d1f7656628132b867f9b8
Caleb Mingle <caleb@mingle.cm>
2021-03-23 23:59:00 -0700

    do some refactors, combine joystick and mpg

---
+++ machine.js
@@ -0,0 +1,38 @@
+class Machine {
+    constructor(socket, port) {
+        this.port = port;
+        this.socket = socket;
+        this.state = {
+            waitingOnOK: false,
+        };
+
+        this.socket.on('serialport:read', function(data) {
+            data = data.trim();
+
+            if(data === 'ok' && this.waitingOnOK) {
+                console.log('waitingOnOK=false');
+
+                this.state.waitingOnOK = false;
+            }
+        });
+    }
+
+    get waitingOnOK() {
+        return Boolean(this.state.waitingOnOK);
+    }
+
+    receivedOK() {
+        this.state.waitingOnOK = false;
+    }
+
+    waitOnOK() {
+        this.state.waitingOnOK = true;
+    }
+
+    writeToCNC(gcode) {
+        console.log('GCODE', gcode.replace(/\r\n|\r|\n/gm, ""));
+        this.socket.emit('write', this.port, gcode);
+    }
+}
+
+module.exports = Machine;
